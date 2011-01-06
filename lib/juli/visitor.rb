c308047b0a563817af84c73fdbb2b7a0327a89da
Fumisky Wells <fwells00 at sourceforge.net>
2011-01-06 20:15:29 +0900

    import all of visitor/*.rb automatically without adding entry anymore here.

--- lib/juli/visitor.rb
+++ lib/juli/visitor.rb
@@ -1,2 +1,4 @@
-require 'juli/visitor/html'
-require 'juli/visitor/tree'
+# import all of visitor/*.rb files
+Dir.glob(File.join(File.dirname(__FILE__), 'visitor/*.rb')){|v|
+  require File.join('juli/visitor', File.basename(v))
+}
