d8ceed250784bc4d6f7b6c06bff59d05ded8ba9f
Fumisky Wells <fwells00 at sourceforge.net>
2011-01-07 17:35:23 +0900

    update doc

--- lib/juli.rb
+++ lib/juli.rb
@@ -1,4 +1,5 @@
 module Juli
+  # sentinel to search juli-repo.  Also it's a directory name to sotre config.
   REPO          = '.juli'
   PKG_ROOT      = File.join(File.dirname(__FILE__), '..')
   TEMPLATE_PATH = File.join(PKG_ROOT, 'lib/template')
