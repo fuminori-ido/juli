1785c36afccac42a4dad84b403abbfd5a0a438dd
Fumisky Wells <fwells00 at sourceforge.net>
2011-01-07 11:26:57 +0900

    move 'usage' method since called from both.

--- lib/juli/command.rb
+++ lib/juli/command.rb
@@ -4,14 +4,24 @@ require 'juli/parser.tab'
 include Juli::Util
 
 module Juli
+  # juli command execution
   module Command
+    # top level command execution.  command_str will be checked and
+    # dispatched to each method.
     def run(command_str, opts)
       case command_str
       when 'gen'
         gen(opts)
+      else
+        STDERR.print "Unknown juli command: '#{command_str}'\n\n", usage, "\n"
+        exit 1
       end
     end
 
+    # generate command
+    #
+    # === OPTIONS
+    # -g generator
     def gen(opts)
       if ARGV.empty?
         print "bulk mode\n"
--- lib/juli/util.rb
+++ lib/juli/util.rb
@@ -1,18 +1,34 @@
 module Juli
   module Util
     def camelize(str)
-  str.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
-end
+      str.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+    end
 
-# Visitor::#{str} constantize
-def visitor(str)
-  camelized = camelize(str)
-  if Visitor.const_defined?(camelized)
-    Visitor.const_get(camelize(str))
-  else
-    raise "Visitor #{camelized} is not defined."
-  end
-end
+    # Visitor::#{str} constantize
+    def visitor(str)
+      camelized = camelize(str)
+      if Visitor.const_defined?(camelized)
+        Visitor.const_get(camelize(str))
+      else
+        raise "Visitor #{camelized} is not defined."
+      end
+    end
 
+    def usage
+      <<EOM
+USAGE: juli [general_options] COMMAND [command_options] [files]
+
+general_options:
+  --help
+  --version
+
+COMMAND (default = gen):
+  gen  
+
+command_options for:
+  gen:
+    -g generator
+EOM
+    end
   end
 end
