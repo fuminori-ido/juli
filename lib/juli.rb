e9b03d977f2f7a05a71809c3067ba68a8eefe7ce
Fumisky Wells <fwells00 at sourceforge.net>
2011-04-07 23:04:40 +0900

    set v1.0.0!

--- lib/juli.rb
+++ lib/juli.rb
@@ -2,7 +2,7 @@ require 'pathname'
 
 # namespace for all of Juli library elements
 module Juli
-  VERSION       = '0.07.00'
+  VERSION       = '1.00.00'
 
   # sentinel to search juli-repo.  Also it's a directory name to sotre config.
   REPO          = '.juli'
