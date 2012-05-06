6d4b2b5e87a609308613fd6dcf7b4219ff6e8abf
Fuminori Ido <ido00 at sourceforge.net>
2012-05-06 12:53:03 +0900

    add tag(macro) unit test.

---
+++ test/macro/tag_test.rb
@@ -0,0 +1,67 @@
+# coding: UTF-8
+
+require 'fileutils'
+require 'test_helper'
+require 'juli'
+require 'juli/util'
+require 'juli/macro'
+
+include Juli::Util
+include Juli::Macro
+
+module Macro
+  class TagTest < Test::Unit::TestCase
+    def setup
+      @saved_cwd = Dir.pwd
+      Dir.chdir(repo4test)
+      Dir.glob('.juli/*.gdbm'){|f| FileUtils.rm_f(f) }
+      @tag = Juli::Macro::Tag.new
+    end
+  
+    def teardown
+      Dir.chdir(@saved_cwd)
+    end
+
+    # rvm ruby-1.9.2-p318 fails this.  I have already reported this issue
+    # to rvm team.  Ruby built from ruby-lang.org works expectedly.
+    def test_gdbm
+      key = 'あいうえおかきくけこ'
+      val = 'たちつてとなにぬねの'
+      @tag.tag_db[key] = val
+      assert_equal val, @tag.tag_db[key].force_encoding(Encoding::UTF_8)
+    end
+
+    def test_new
+      assert @tag
+    end
+
+    def test_on_root
+      @tag.page_db['t001'] = ''
+      assert_equal      '', @tag.page_db['t001']
+      @tag.on_root('t001.txt', nil)
+      assert_not_equal  '', @tag.page_db['t001']
+    end
+
+    def test_run
+      key = sprintf("%s%s%s", 't001', Juli::Macro::Tag::SEPARATOR, 'DIY')
+      @tag.on_root('t001.txt', nil)
+      @tag.tag_db['DIY']    = ''
+      @tag.tag_page_db[key] = ''
+      assert_equal '', @tag.run('DIY')
+      assert_not_equal  '', @tag.tag_db['DIY']
+      assert_not_equal  '', @tag.tag_page_db[key]
+    end
+
+    def test_pages
+      @tag.on_root('t001.txt', nil)
+      @tag.run('DIY', 'DOG')
+
+      @tag.on_root('t002.txt', nil)
+      @tag.run('DIY', 'CAT')
+
+      assert_equal ['t001', 't002'],  @tag.pages('DIY').sort
+      assert_equal ['t001'],          @tag.pages('DOG').sort
+      assert_equal ['t002'],          @tag.pages('CAT').sort
+    end
+  end
+end
