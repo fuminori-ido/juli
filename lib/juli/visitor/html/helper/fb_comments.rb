fd5e31a98a2228a7243c3f13d83d2601adb7c9d1
Fuminori Ido <ido00 at sourceforge.net>
2012-05-14 09:24:58 +0900

    add 'facebook comments' helper

---
+++ lib/juli/visitor/html/helper/fb_comments.rb
@@ -0,0 +1,33 @@
+module Juli::Visitor::Html::Helper
+  # Helper-class for 'fb_like' helper
+  class FbComments < AbstractHelper
+    # default HTML template for facebook 'like' button.
+    # You can customize it in .juli/config facebook.like.template entry.
+    #
+    # %s in the template will be replaced to the actual URL of
+    # current wiki page.
+    TEMPLATE =
+      '<fb:comments href="%s" num_posts="2" width="470">' +
+      '</fb:comments>'
+
+    def initialize
+      @fb_conf  = conf['facebook']
+    end
+
+    # called on each parsed document
+    def on_root(in_file, root)
+      @in_file  = in_file
+    end
+
+    def run(*args)
+      sprintf(template, conf['url_prefix'] + '/' + 
+          to_wikiname(@in_file) + conf['ext'])
+    end
+
+  private
+    def template
+      @fb_conf && @fb_conf['comments'] && @fb_conf['comments']['template'] ||
+      TEMPLATE
+    end
+  end
+end
---
+++ test/visitor/html/helper/fb_comments_test.rb
@@ -0,0 +1,47 @@
+require 'fileutils'
+require 'test_helper'
+require 'juli'
+require 'juli/util'
+require 'juli/visitor'
+
+include Juli::Util
+
+# NOTE: Following module name cannot be Juli::Visitor::Html::Helper because
+# if so, FbCommentsTest would become a *WRONG HELPER* under
+# Juli::Visitor::Html::Helper and failed at testing.
+module JuliUnitTest
+  class FbCommentsTest < Test::Unit::TestCase
+    def setup
+      @saved_cwd = Dir.pwd
+      Dir.chdir(repo4test)
+      Dir.glob('.juli/*.gdbm'){|f| FileUtils.rm_f(f) }
+      @fb_comments = Juli::Visitor::Html::Helper::FbComments.new
+    end
+  
+    def teardown
+      Dir.chdir(@saved_cwd)
+    end
+
+    def test_new
+      assert @fb_comments
+    end
+
+    def test_template
+      # template in .juli/config
+      assert_equal '<fb:comments href="%s"></fb:comments>', @fb_comments.send(:template)
+
+      # default template
+      saved = conf['facebook']['comments']['template']
+        conf['facebook']['comments']['template'] = nil
+        assert_match /width=/, @fb_comments.send(:template)          #/
+      conf['facebook']['comments']['template'] = saved
+    end
+
+    def test_run
+      @fb_comments.on_root('a.txt', nil)
+      assert_equal(
+          '<fb:comments href="http://local:PORT/JULI/a.html"></fb:comments>',
+          @fb_comments.run)
+    end
+  end
+end
